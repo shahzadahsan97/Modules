014a26a50dc35b53c1c7b6f8ca08b994efb30a42
Shahzad Ahsan <dankara97@gmail.com>
2019-09-19 16:54:45 +0500

    making modules(function)

---
+++ Make_modules.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+def add(a,b):
+    c = a + b
+    return  c
+
+
+# In[2]:
+
+
+def sub(a,b):
+    x = a - b
+    return x
+
+
+# In[3]:
+
+
+def mult(a,b):
+    z = a * b
+    return z
+
+
+# In[4]:
+
+
+def divide(a,b):
+    y = a * b
+    return y
+
